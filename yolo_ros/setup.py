17eb20037f4276afe487fe0aa33ce2ade392baa5
Fernandoufop <fernando_rodrigues1990@hotmail.com>
2026-01-22 14:26:59 -0300

    Rename package from yolo_ros to host-yolo
    Signed-off-by: Fernandoufop <fernando_rodrigues1990@hotmail.com>

--- yolo_ros/setup.py
+++ yolo_ros/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = "yolo_ros"
+package_name = "host-yolo"
 
 setup(
     name=package_name,
@@ -12,17 +12,15 @@ setup(
     ],
     install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="Miguel Ángel González Santamarta",
-    maintainer_email="mgons@unileon.es",
     description="YOLO for ROS 2",
     license="GPL-3.0",
     extras_require={"test": ["pytest"]},
     entry_points={
         "console_scripts": [
-            "yolo_node = yolo_ros.yolo_node:main",
-            "debug_node = yolo_ros.debug_node:main",
-            "tracking_node = yolo_ros.tracking_node:main",
-            "detect_3d_node = yolo_ros.detect_3d_node:main",
+            "yolo_node = host-yolo.yolo_node:main",
+            "debug_node = host-yolo.debug_node:main",
+            "tracking_node = host-yolo.tracking_node:main",
+            "detect_3d_node = host-yolo.detect_3d_node:main",
         ],
     },
 )
